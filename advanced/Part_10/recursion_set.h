ef8265276387384d3b650f553b3b979dec247f61
kimkj2013 <kimk3@miamioh.edu>
2015-10-12 23:09:14 -0400

    recursion

--- advanced/Part_10/recursion_set.h
+++ advanced/Part_10/recursion_set.h
@@ -1,8 +1,53 @@
-#ifndef RECURSION_SET_H_
+//#ifndef RECURSION_SET_H_
 #define RECURSION_SET_H_
 
-int exponential(int n) {
+/*
+ * Implement the recursive function to compute the nth power of base.
+ * For example, exponential(2, 0) is 2^0 (2 to the power of 0), so it is 1.
+ */
+int exponential(int base, int n) {
+  if (n == 0) return 1;
+  return n * exponential(base, n - 1);
+}
+
+/*
+ * Implement the recursive function to compute the log x with base.
+ * For example, logarithm(2, 1) is log(1), base 2, so it is 0.
+ */
+int logarithm(int base, int x) {
   return -1;
 }
 
-int logarithm 
+/*
+ * Sum all the digits for a given number n.
+ * For example, sum_of_digit(123) = 1 + 2 + 3 = 6.
+ */
+int sum_of_digit(int n) {
+  return -1;
+}
+
+/*
+ * Count how many 6's exist for a given number n.
+ * For example, count_six(456) = 1, and count_six(666) = 3.
+ */
+int count_six(int n) {
+  return -1;
+}
+
+/*
+ * Find the greatest common divisor of x and y.
+ */
+int gcd(int x, int y) {
+  return -1;
+}
+
+/*
+ * Determine how many 'steps' are needed for a given number n to be zero.
+ * 'Steps' are defined as the number of divisions by 2.
+ * For example, if n = 9, you will need 5 'steps' to reach 0.
+ * Sequence: 9, 4, 2, 1, 0
+ * Ignore remainders.
+ */
+int div2(int n) {
+  return -1;
+}
--- advanced/Part_10/recursion_test.c
+++ advanced/Part_10/recursion_test.c
@@ -3,12 +3,33 @@
 #include <iostream>
 #include "recursion_set.h"
 
+void print_pass() {
+  std::cout << "PASS" << std::endl;
+}
+
+void print_fail() {
+  std::cout << "FAIL" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
   if (argc > 1) { // If there is a test argument
 
   }
   else { // Normal mode
-
+    std::cout << "Testing exponential... ";
+    if (exponential(2, 1) != 2) {
+      print_fail();
+      return 0;
+    }
+    if (exponential(3, 4) != 81) {
+      print_fail();
+      return 0;
+    }
+    if (exponential(6, 3) != 216) {
+      print_fail();
+      return 0;
+    }
+    print_pass();
   }
   return 0;
 }
